3939458830225de9e4844034b68dd3a325866896
vusalasalimovaa <salimovavusala7@gmail.com>
2024-12-17 13:54:52 +0400

    state

--- 3.react-state/src/components/Modal/index.jsx
+++ 3.react-state/src/components/Modal/index.jsx
@@ -13,6 +13,8 @@ const Modal = () => {
 
   <div id="id01" className="w3-modal">
     <div className="w3-modal-content">
+
+      
       <div className="w3-container">
         <span onClick={toogle} className="w3-button w3-display-topright">&times;</span>
         <p>Some text. Some text. Some text.</p>
